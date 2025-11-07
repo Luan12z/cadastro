856ee2398ab1dfcdd219b3fa6f35399ad628322e
Luan12z <luan.lara.otto@escola.pr.gov.br>
2025-11-06 21:12:19 -0300

    Add files via upload

---
+++ cadastro.js
@@ -0,0 +1,16 @@
+const primeiroNome = "luan";  
+const sobrenome = "felipe";    
+let idade = 17;               
+
+const nomeCompleto = `${primeiroNome} ${sobrenome}`;
+
+
+console.log(`Nome completo do usuário: ${nomeCompleto}`);
+console.log(`idade do usuário: ${idade}`);
+
+idade++;
+console.log(`Aniversário do Usuário: ${idade}`);
+
+
+console.log(`Nova idade após aniversário: ${idade}`);
+
